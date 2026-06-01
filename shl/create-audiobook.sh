7a14701472712edf9ea0c92fb6e790446ddd2d62
Your Name <you@example.com>
2026-06-01 16:49:05 +0500

    Simplify old audiobook script

--- shl/create-audiobook.sh
+++ shl/create-audiobook.sh
@@ -4,4 +4,4 @@ model="~/src/piper/models/en_US-kathleen-low.onnx"
 rawBook=$(mktemp)
 
 # First argument must be an epub file
-(epub2txt "$1" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" >> $rawBook) && piper-tts --model $model --output-file "audiobook-$(uuidgen).wav" < $rawBook
+(epub2txt -n "$1" >> $rawBook) && piper-tts --model $model --output-file "audiobook-$(uuidgen).wav" < $rawBook
